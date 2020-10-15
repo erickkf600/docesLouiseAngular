35f7e056c4d5d45f29ba8c7d4ae69504a668f233
Erick <erickkf600@gmail.com>
2020-10-15 19:21:41 -0300

    #waves

--- src/app/pages/home/modules/home-container/home-container.module.ts
+++ src/app/pages/home/modules/home-container/home-container.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HomeContainerRoutingModule } from './home-container-routing.module';
 import { HomeContainerComponent } from './home-container.component';
+import { ViewHomeComponent } from './view/view-home/view-home.component';
 
 @NgModule({
-  declarations: [HomeContainerComponent],
+  declarations: [HomeContainerComponent, ViewHomeComponent],
   imports: [
     CommonModule,
     HomeContainerRoutingModule
